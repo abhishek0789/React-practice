bb69000ea77bc3ebc31c5f26426002430b0a0ecc
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-10-01 08:15:26 +0000

    commit 2

---
+++ Blog/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='bg-red-400 text-green-300'>
+        <li>
+            Blog
+        </li>
+        <li>
+            Home
+        </li>
+    </div>
+  )
+}
+
+export default Home
